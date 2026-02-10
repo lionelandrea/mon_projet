caf3f03a5ce0ac64528b0e731aa732887a9a6746
unknown <lionelandrea>
2026-02-10 12:36:15 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+   return (
+    <div className="container">
+      <h1>Mon Portfolio</h1>
+
+      <p>
+        Bienvenue sur mon portfolio ! Ici vous pouvez découvrir mes projets, mes
+        compétences et mon parcours.
+      </p>
+
+      <img src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr1Qt6q63vjccs0DdpWzJQo-1APd0d07ywhA&s" alt="Mon Portfolio" className="portfolio-image" />
+      <br />
+
+      <button className="btn-contact">Me contacter</button>
+    </div>
+  );
+}
+
+export default App;
